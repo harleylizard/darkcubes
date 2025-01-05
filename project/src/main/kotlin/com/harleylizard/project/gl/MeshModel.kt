05ccd47a45b61895347799f9fc38d6409972bbe5
harleylizard <64570529+harleylizard@users.noreply.github.com>
2025-01-06 03:48:52 +1000

    im so sleepy

--- project/src/main/kotlin/com/harleylizard/project/gl/MeshModel.kt
+++ project/src/main/kotlin/com/harleylizard/project/gl/MeshModel.kt
@@ -3,6 +3,7 @@ package com.harleylizard.project.gl
 import com.harleylizard.project.Resources.readEntityModel
 import com.harleylizard.project.material.Material
 import com.harleylizard.project.model.MeshBuilder
+import com.harleylizard.project.model.entity.FlatSize
 import org.joml.Matrix4f
 import org.lwjgl.opengl.GL45.*
 
@@ -19,7 +20,7 @@ class MeshModel {
 			val empty = Matrix4f()
 			val model = "assets/model/entity/player_wide_arms.json".readEntityModel
 
-			model.build(empty, it, Material(64, 64))
+			model.build(empty, it, FlatSize(64, 64))
 			it.triangulate()
 
 			count = it.count

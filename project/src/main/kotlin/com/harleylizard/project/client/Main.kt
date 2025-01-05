d4155444188135d9a56ae255e2c02d6bcd974f1f
harleylizard <64570529+harleylizard@users.noreply.github.com>
2025-01-05 18:22:45 +1000

    glfw Window

--- project/src/main/kotlin/com/harleylizard/project/client/Main.kt
+++ project/src/main/kotlin/com/harleylizard/project/client/Main.kt
@@ -1,9 +1,32 @@
 package com.harleylizard.project.client
 
+import org.lwjgl.glfw.GLFW.glfwInit
+import org.lwjgl.glfw.GLFW.glfwTerminate
+import org.lwjgl.opengl.GL11.*
+import org.lwjgl.system.MemoryUtil.memAddress
+import java.nio.Buffer
+
 object Main {
 
 	@JvmStatic
 	fun main(args: Array<String>) {
+		throwIf(::RuntimeException) { !glfwInit() }
 
+		val window = Window(854, 480)
+
+		glClearColor(0.0F, 0.75F, 0.75F, 0.0F)
+		while (!window.shouldClose) {
+			glClear(GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT)
+			window.update()
+		}
+		window.destroy()
+
+		glfwTerminate()
 	}
+
+	inline fun <T, E : Exception> T.throwIf(unit: () -> E, predicate: (T) -> Boolean) {
+		if (predicate(this)) throw unit()
+	}
+
+	inline val Buffer.memAddress; get() = memAddress(this)
 }
---
+++ project/src/main/kotlin/com/harleylizard/project/client/Window.kt
@@ -0,0 +1,52 @@
+package com.harleylizard.project.client
+
+import com.harleylizard.project.client.Main.memAddress
+import com.harleylizard.project.client.Main.throwIf
+import org.lwjgl.glfw.Callbacks.glfwFreeCallbacks
+import org.lwjgl.glfw.GLFW.*
+import org.lwjgl.opengl.GL
+import org.lwjgl.system.MemoryStack
+import org.lwjgl.system.MemoryUtil.NULL
+
+class Window(width: Int, height: Int) {
+	private val window: Long
+
+	val shouldClose; get() = glfwWindowShouldClose(window)
+
+	init {
+		glfwDefaultWindowHints()
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4)
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6)
+		glfwWindowHint(GLFW_CONTEXT_RELEASE_BEHAVIOR, GLFW_RELEASE_BEHAVIOR_FLUSH)
+		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE)
+
+		glfwCreateWindow(width, height, "Project", NULL, NULL).also { window = it }.throwIf(::RuntimeException) { it == NULL }
+		MemoryStack.stackPush().use {
+			val buffer = it.callocInt(6)
+
+			nglfwGetMonitorWorkarea(glfwGetPrimaryMonitor(),
+				buffer.memAddress,
+				buffer.memAddress + 4,
+				buffer.memAddress + 8,
+				buffer.memAddress + 12)
+			nglfwGetWindowSize(window, buffer.memAddress + 16, buffer.memAddress + 20)
+
+			val x = (buffer[2] - (buffer[4])) / 2
+			val y = (buffer[3] - (buffer[5])) / 2
+			glfwSetWindowPos(window, x, y)
+		}
+
+		glfwMakeContextCurrent(window)
+		GL.createCapabilities()
+	}
+
+	fun update() {
+		glfwSwapBuffers(window)
+		glfwPollEvents()
+	}
+
+	fun destroy() {
+		glfwFreeCallbacks(window)
+		glfwDestroyWindow(window)
+	}
+}
